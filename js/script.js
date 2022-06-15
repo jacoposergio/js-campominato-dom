5a4f4306ae4e489d6ec5d5e849eace609b61a0fd
jacoposergio <djago32@gmail.com>
2022-06-15 16:15:19 +0200

    start

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-alert('ciao');
+// TODO|L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in /// TODO|cui ogni cella contiene un numero tra quelli compresi in un range:
+// TODO|con difficoltà 1 => tra 1 e 100
+// TODO|con difficoltà 2 => tra 1 e 81
+// TODO|con difficoltà 3 => tra 1 e 49
+// TODO|Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// TODO|I numeri nella lista delle bombe non possono essere duplicati.
+// TODO|In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// TODO|La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+// TODO|Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+// TODO|BONUS:
+// TODO|1- quando si clicca su una bomba e finisce la partita, evitare che si possa cliccare su altre celle
+// TODO|2- quando si clicca su una bomba e finisce la partita, il software scopre tutte le bombe nascoste
+
